d3dc21bf6c21471fbf7b0a21fdef1e9a03182cc5
Brian Bycynski <high5bri@hotmail.com>
2018-07-21 08:48:33 -0400

    finished lecture 12

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://brian:mollymoo72@ds143451.mlab.com:43451/devconnector'
+    mongoURI: 'mongodb://brian:mollymoo72@ds143451.mlab.com:43451/devconnector',
+    secretOrKey: 'secret'
 };
