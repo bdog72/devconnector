56125b9ed30d6bc28ffbbc3bdd3060ed3cdd554d
Brian Bycynski <high5bri@hotmail.com>
2018-07-22 09:09:27 -0400

    finished lecture 18

--- server.js
+++ server.js
@@ -35,4 +35,4 @@ app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Serever is running on port ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
