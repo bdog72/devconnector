b577110d3fd9e297d149aa0a9fe367031959cc1f
Brian Bycynski <high5bri@hotmail.com>
2018-07-23 06:51:03 -0400

    finished lecture 20

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Load Validation
+const validateProfileInput = require('../../validation/profile');
+
 // Load profile model
 const Profile = require('../../models/Profile');
 
@@ -23,6 +26,7 @@ router.get(
     (req, res) => {
         const errors = {};
         Profile.findOne({ user: req.user.id })
+            .populate('user', ['name', 'avatar'])
             .then(profile => {
                 if (!profile) {
                     errors.noProfile = 'There is no profile for this user';
@@ -41,6 +45,14 @@ router.post(
     '/',
     passport.authenticate('jwt', { session: false }),
     (req, res) => {
+        const { errors, isValid } = validateProfileInput(req.body);
+
+        // Check validation
+        if (!isValid) {
+            // Return any erros with a 400 status
+            return res.status(400).json(errors);
+        }
+
         // Get Fields
         const profileFields = {};
         profileFields.user = req.user.id;
@@ -68,7 +80,7 @@ router.post(
         if (req.body.instagram)
             profileFields.social.instagram = req.body.instagram;
 
-        Profiel.findOne({ user: req.user.id }).then(profile => {
+        Profile.findOne({ user: req.user.id }).then(profile => {
             if (profile) {
                 // Update
                 Profile.findOneAndUpdate(
