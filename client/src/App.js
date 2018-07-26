964b878cb9ede52c8e05f337bf44aaa775e1a75a
Brian Bycynski <high5bri@hotmail.com>
2018-07-26 13:02:32 -0400

    finished lecture 46

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -11,6 +12,7 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 import './App.css';
 
 // Check for token
@@ -28,6 +30,7 @@ if (localStorage.jwtToken) {
         // Logout user
         store.dispatch(logoutUser());
         // TODO: Clear current profile
+        store.dispatch(clearCurrentProfile());
 
         // Redirect to login
         window.location.href = '/login';
@@ -49,6 +52,11 @@ class App extends Component {
                                 component={Register}
                             />
                             <Route exact path="/login" component={Login} />
+                            <Route
+                                exact
+                                path="/dashboard"
+                                component={Dashboard}
+                            />
                         </div>
                         <Footer />
                     </div>
