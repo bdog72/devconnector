0a3c799e92eb773835b82ed91c65dc273ebb5403
Brian Bycynski <high5bri@hotmail.com>
2018-07-26 06:29:45 -0400

    finished lecture 44

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -48,3 +48,14 @@ export const setCurrentUser = decoded => {
         payload: decoded
     };
 };
+
+// Log user out
+
+export const logoutUser = () => dispatch => {
+    // Rmove token from localstorage
+    localStorage.removeItem('jwtToken');
+    // Remove auth header for future requests
+    setAuthToken(false);
+    // Set current user to {} which will set isAuthenticated to false
+    dispatch(setCurrentUser({}));
+};
