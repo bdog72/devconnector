88b1b127157a9cfcc67c40454bacac53174f04d9
Brian Bycynski <high5bri@hotmail.com>
2018-07-28 06:02:10 -0400

    finished lecture 52

--- client/src/components/create-Profile/CreateProfile.jsx
+++ client/src/components/create-Profile/CreateProfile.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import InputGroup from '../common/InputGroup';
 import SelectListGroup from '../common/SelectListGroup';
 import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
+import { createProfile } from '../../actions/profileActions';
 
 class CreateProfile extends Component {
     constructor(props) {
@@ -30,9 +32,30 @@ class CreateProfile extends Component {
         this.onChange = this.onChange.bind(this);
     }
 
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({ errors: nextProps.errors });
+        }
+    }
+
     onSubmit(e) {
         e.preventDefault();
-        console.log('Submitted Form');
+        const profileData = {
+            handle: this.state.handle,
+            company: this.state.company,
+            website: this.state.website,
+            location: this.state.location,
+            status: this.state.status,
+            skills: this.state.skills,
+            githubusername: this.state.githubusername,
+            bio: this.state.bio,
+            twitter: this.state.twitter,
+            facebook: this.state.facebook,
+            linkedin: this.state.linkedin,
+            youtube: this.state.youtube,
+            instagram: this.state.instagram
+        };
+        this.props.createProfile(profileData, this.props.history);
     }
 
     onChange(e) {
@@ -220,6 +243,7 @@ class CreateProfile extends Component {
                                 />
                                 <div className="mb-3">
                                     <button
+                                        type="button"
                                         onClick={() => {
                                             this.setState(prevState => ({
                                                 displaySocialInputs: !prevState.displaySocialInputs
@@ -246,14 +270,17 @@ class CreateProfile extends Component {
     }
 }
 
-// CreateProfile.propTypes = {
-//     profile: PropTypes.object.isRequired,
-//     errors: PropTypes.object.isRequired
-// };
+CreateProfile.propTypes = {
+    profile: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
     profile: state.profile,
     errors: state.errors
 });
 
-export default connect(mapStateToProps)(CreateProfile);
+export default connect(
+    mapStateToProps,
+    { createProfile }
+)(withRouter(CreateProfile));
