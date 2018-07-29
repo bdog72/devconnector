5dbc37801e9d29c519ebae3d519b0117d9704108
Brian Bycynski <high5bri@hotmail.com>
2018-07-29 06:10:30 -0400

    finished lecture 55

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-Profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
 import './App.css';
 
 // Check for token
@@ -75,6 +76,13 @@ class App extends Component {
                                     exact
                                     path="/edit-profile"
                                     component={EditProfile}
+                                />
+                            </Switch>
+                            <Switch>
+                                <PrivateRoute
+                                    exact
+                                    path="/add-experience"
+                                    component={AddExperience}
                                 />
                             </Switch>
                         </div>
