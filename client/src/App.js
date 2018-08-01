f7cf0110eb2092876a42e1ac7292542c5d042790
Brian Bycynski <high5bri@hotmail.com>
2018-08-01 07:15:22 -0400

    finished lecture 64

--- client/src/App.js
+++ client/src/App.js
@@ -25,93 +25,77 @@ import './App.css';
 
 // Check for token
 if (localStorage.jwtToken) {
-    // Set auth token header auth
-    setAuthToken(localStorage.jwtToken);
-    // Decode token and get user info and expiration
-    const decoded = jwt_decode(localStorage.jwtToken);
-    // Set user and isAuthenticated
-    store.dispatch(setCurrentUser(decoded));
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and expiration
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
 
-    // Check for expired token
-    const currentTime = Date.now() / 1000;
-    if (decoded.exp < currentTime) {
-        // Logout user
-        store.dispatch(logoutUser());
-        // TODO: Clear current profile
-        store.dispatch(clearCurrentProfile());
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // TODO: Clear current profile
+    store.dispatch(clearCurrentProfile());
 
-        // Redirect to login
-        window.location.href = '/login';
-    }
+    // Redirect to login
+    window.location.href = '/login';
+  }
 }
 
 class App extends Component {
-    render() {
-        return (
-            <Provider store={store}>
-                <Router>
-                    <div>
-                        <Navbar />
-                        <Route exact path="/" component={Landing} />
-                        <div className="container">
-                            <Route
-                                exact
-                                path="/register"
-                                component={Register}
-                            />
-                            <Route exact path="/login" component={Login} />
-                            <Route
-                                exact
-                                path="/profiles"
-                                component={Profiles}
-                            />
-                            <Route
-                                exact
-                                path="/profile/:handle"
-                                component={Profile}
-                            />
-                            <Switch>
-                                <PrivateRoute
-                                    exact
-                                    path="/dashboard"
-                                    component={Dashboard}
-                                />
-                            </Switch>
-                            <Switch>
-                                <PrivateRoute
-                                    exact
-                                    path="/create-profile"
-                                    component={CreateProfile}
-                                />
-                            </Switch>
-                            <Switch>
-                                <PrivateRoute
-                                    exact
-                                    path="/edit-profile"
-                                    component={EditProfile}
-                                />
-                            </Switch>
-                            <Switch>
-                                <PrivateRoute
-                                    exact
-                                    path="/add-experience"
-                                    component={AddExperience}
-                                />
-                            </Switch>
-                            <Switch>
-                                <PrivateRoute
-                                    exact
-                                    path="/add-education"
-                                    component={AddEducation}
-                                />
-                            </Switch>
-                        </div>
-                        <Footer />
-                    </div>
-                </Router>
-            </Provider>
-        );
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div>
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-education"
+                  component={AddEducation}
+                />
+              </Switch>
+            </div>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
